5b7b07e74a7c39f677c142e1605aa288205e0abb
Paulette Currington <paulette.currington@gmail.com>
2024-05-13 20:12:05 -0500

    feat: added clock and more butters actions

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import './App.css';
-import ButtersBlinkingSlow from '../components/ButtersBlinkingSlow';
-import ButtersBlinkingQuick from '../components/ButtersBlinkingQuick';
+// import ButtersBlinkingSlow from '../components/ButtersBlinkingSlow';
+// import ButtersBlinkingQuick from '../components/ButtersBlinkingQuick';
+import ButtersSleeping from '../components/ButtersSleeping'
+import ButtersBreathingSleepingTorso from '../components/ButtersBreathingSleepingTorso';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </header>
-      <ButtersBlinkingSlow frameCount={4} frameRate={3} />
-      <ButtersBlinkingQuick frameCount={4} frameRate={3} />
+      {/* <ButtersBlinkingSlow frameCount={4} frameRate={3} /> */}
+      {/* <ButtersBlinkingQuick frameCount={4} frameRate={3} /> */}
+      <div>
+        <div className='butters-sleeping'>
+          <ButtersSleeping />
+        </div>
+        <div className='butters-breathing-sleeping-torso'>
+          <ButtersBreathingSleepingTorso />
+        </div>
+      </div>
+
     </div>
   );
 }
