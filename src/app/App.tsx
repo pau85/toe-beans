fee0082738f43f1be580049131f84dccdbdc778c
Paulette Currington <paulette.currington@gmail.com>
2024-08-22 01:45:36 -0500

    feat: added Router for application

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import { HashRouter as Router } from 'react-router-dom'; 
 import '../styles/App.scss';
 import ButtersSleeping from '../components/characters/butters/buttersSleepingModule/ButtersSleeping';
-// import ClockWithHands from '../components/items/clockwithands/ClockWithHands';
 import LivingRoom from '../components/backgroundImages/LivingRoom';
 import DiningRoom from '../components/backgroundImages/DiningRoom'
 import ButtersBreathingSleepingTorso from '../components/characters/butters/buttersSleepingModule/ButtersBreathingSleepingTorso';
@@ -9,24 +9,23 @@ import Clock from '../components/items/clockwithands/clock'
 function App() {
 
   return (
-    <div>
-      {/* <div className="clock-item">
-        <ClockWithHands />
-      </div> */}
-      <div className="livingroom">
-        <LivingRoom />
+    <Router>
+      <div>
+        <div className="livingroom">
+          <LivingRoom />
+        </div>
+        <div className="diningroom">
+          <DiningRoom />
+        </div>
+        <div className="full-clock"><Clock /></div>
+        <div className="butters-character-breathing">
+          <ButtersBreathingSleepingTorso />
+        </div>
+        <div className="butters-character-sleeping">
+          <ButtersSleeping />
+        </div>
       </div>
-      <div className="diningroom">
-        <DiningRoom />
-      </div>
-      <div className="full-clock"><Clock /></div>
-      <div className="butters-character-breathing">
-        <ButtersBreathingSleepingTorso />
-      </div>
-      <div className="butters-character-sleeping">
-        <ButtersSleeping />
-      </div>
-    </div>
+    </Router>
   );
 }
 
