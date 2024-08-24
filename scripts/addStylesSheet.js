45c56361366821ebc2d0ce1b94de160a571dbc50
Paulette Currington <paulette.currington@gmail.com>
2024-08-24 15:38:41 -0500

    feat: added script to automatically compile scss files with gulp before builds, deploys and start commands

--- scripts/addStylesSheet.js
+++ scripts/addStylesSheet.js
@@ -1,34 +1,73 @@
 const fs = require('fs');
 const path = require('path');
+const readline = require('readline')
 
 // Directory with your .scss files
-const scssDir = '../src/styles/';
-const scssClockDir = '../src/styles/clock/'
+const scssStylesDir = './src/styles/';
 
-const scssDirs = [scssDir, scssClockDir]
+const scssDirs = [scssStylesDir]
 
-for (let index = 0; index < scssDirs.length; index++) {
+async function processFiles() {
+  for (let index = 0; index < scssDirs.length; index++) {
     const scssDir = scssDirs[index];
+    let content = '';
+    let newFilesAdded = false; // Add this line
+    const files = fs.readdirSync(scssDir);
+      
+    const promises = files.map(async file => {
+      if (path.extname(file) === '.scss') {
+        // Remove the file extension
+        let filename = path.basename(file, '.scss');
+        if (filename === 'styles') { return }
+        let dirpath = scssDir
+        let extension = path.extname(file)
 
-    fs.readdir(scssDir, (err, files) => {
-        if (err) throw err;
-      
-        let content = '';
-      
-        files.forEach(file => {
-          if (path.extname(file) === '.scss') {
-            // Remove the file extension
-            let filename = path.basename(file, '.scss');
-            let dirpath = scssDir
-            let extension = path.extname(file)
-            // Add the @use directive
-            content += `@use '${dirpath}${filename}${extension}';\n`;
-          }
-        });
-      
-        fs.writeFile(path.join(scssDir, 'styles.scss'), content, err => {
-          if (err) throw err;
-          console.log('styles.scss has been updated!');
-        });
-      });
+        let fileAlreadyAdded = await isFileAlreadyAdded(dirpath + filename + extension)
+        if (fileAlreadyAdded === false) {
+          dirpath = scssDir
+          extension = path.extname(file)
+          // Add the @use directive
+          content += `@use '${dirpath}${filename}${extension}';\n`;
+          newFilesAdded = true; // Update the flag
+        }
+      }
+    });
+
+    await Promise.all(promises);
+    if (newFilesAdded){
+      fs.appendFileSync(path.join(scssDir, 'styles.scss'), content);
+      console.log('styles.scss has been updated!');
+    } else {
+      console.log('No new .scss files to compile in "' +scssDir+'"!');
+    }
+  }
+}
+
+processFiles();
+
+function isFileAlreadyAdded(filename) {
+  return new Promise((resolve, reject) => {  
+    const fileStream = fs.createReadStream('./src/styles/styles.scss');
+
+    // Use readline to read from the stream
+    const rl = readline.createInterface({
+      input: fileStream,
+      crldDelay: Infinity
+    });
+
+    // Listen for 'line' events and log each line to the console
+    rl.on('line', (line) => {
+      if (line === (`@use '${filename}';`)) {   
+        resolve(true)
+      }
+    })
+
+    rl.on('close', () => {
+      resolve(false);
+    });
+
+    rl.on('error', (err) => {
+      reject(err);
+    });
+  })
 }
