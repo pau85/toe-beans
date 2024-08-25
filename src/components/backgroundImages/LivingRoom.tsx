f6f3bbfefad2e10ff805cee76b923a02195a4f5b
Paulette Currington <paulette.currington@gmail.com>
2024-08-25 18:26:24 -0500

    feat: changed the way livingroom is rendered

--- src/components/backgroundImages/LivingRoom.tsx
+++ src/components/backgroundImages/LivingRoom.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
-import frame1 from '../../images/backgroundImages/livingroom-background-with-couch.png';
 import '../../styles/Livingroom.scss'
   const LivingRoom = () => {
   
-    // Function to get the image source based on the current frame
-    const getImageSrc = () => {
-      return frame1;
-      }
-  
     return (
-      <img src={getImageSrc()} alt="living-room" className="living-room"/>
+      <div className="living-room">
+      </div>
     );
   };
   
