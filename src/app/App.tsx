891d8f2d5fabd0b0f44f20b4d95bfcd6e8138399
Paulette Currington <paulette.currington@gmail.com>
2024-08-25 19:18:47 -0500

    feat: added diningroom

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,8 +3,8 @@ import { HashRouter as Router } from 'react-router-dom';
 import '../styles/App.scss';
 import LivingRoom from '../components/backgroundImages/LivingRoom.tsx';
 import DiningRoom from '../components/backgroundImages/DiningRoom.tsx'
-import Clock from '../components/items/clockwithands/clock.tsx'
-import SleepingButters from '../components/characters/butters/buttersSleepingModule/SleepingButters.tsx';
+//import Clock from '../components/items/clockwithands/clock.tsx'
+//import SleepingButters from '../components/characters/butters/buttersSleepingModule/SleepingButters.tsx';
 function App() {
 
   return (
@@ -13,13 +13,13 @@ function App() {
         <div className="livingroom">
           <LivingRoom />
         </div>
-        {/* <div className="diningroom">
+        <div className="diningroom">
           <DiningRoom />
         </div>
-        <div className="full-clock"><Clock /></div>
+        {/* <div className="full-clock"><Clock /></div>
         <div className='sleeping-butters'>
           <SleepingButters />
-        </div> */}
+        </div> */} 
       </div>
     </Router>
   );
--- src/components/backgroundImages/DiningRoom.tsx
+++ src/components/backgroundImages/DiningRoom.tsx
@@ -1,15 +1,9 @@
 import React from 'react';
-import frame1 from '../../images/backgroundImages/dining-room.png';
 import '../../styles/Diningroom.scss'
   const DiningRoom = () => {
   
-    // Function to get the image source based on the current frame
-    const getImageSrc = () => {
-      return frame1;
-      }
-  
     return (
-      <img src={getImageSrc()} alt="dining-room" className="dining-room"/>
+      <div className="diningroom"></div>
     );
   };
   
